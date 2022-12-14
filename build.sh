9f4b1ffcaf46e0a79275c08f71047c5be6d71824
Hekmon <edouardhur@gmail.com>
2022-12-14 12:42:37 +0100

    add build script

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+crosscompile () {
+    version=$(date +%Y%m%d)
+    GOOS="$1" GOARCH="$2" go build -v -ldflags="-X 'main.Version=${version}'" -o 'edfbleu'
+    zip -9 "edfbleu_${version}_${1}_${2}.zip" 'edfbleu'
+}
+
+echo '* Compiling for Windows'
+crosscompile 'windows' 'amd64'
+echo
+echo '* Compiling for MacOS Intel'
+crosscompile 'darwin' 'amd64'
+echo
+echo '* Compiling for MacOS Apple Silicon'
+crosscompile 'darwin' 'arm64'
+echo
+echo '* Compiling for Linux'
+crosscompile 'linux' 'amd64'
+echo
+echo '* Cleaning up'
+test -f edfbleu && rm edfbleu
+test -f edfbleu.exe && rm edfbleu.exe
